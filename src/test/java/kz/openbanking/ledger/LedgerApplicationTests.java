4e03a560894fd05bf1590568496c435f4b90d680
edikhanbayev <dikhanbayev@gmail.com>
2026-09-07 20:42:44 +0500

    Spring boot ledger project

---
+++ src/test/java/kz/openbanking/ledger/LedgerApplicationTests.java
@@ -0,0 +1,13 @@
+package kz.openbanking.ledger;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LedgerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
